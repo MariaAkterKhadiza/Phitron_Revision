4b0a17cded6e861e36cf247fb66f7da9eca13b4d
MariaAkterKhadiza <khadizatulmaria2002@gmail.com>
2024-06-14 13:41:49 +0600

    First commit

---
+++ C#/conceptual_session_1-1/operation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+      int a =3;
+      int b = 10;
+      int sum = a+b;
+      printf("%d",sum);
+    //   int sum2 = a-b;
+    //   printf("%d",sum);
+    //   int mod = a%b;
+    //   printf("%d",mod);
+
+    //   printf("%d",sum2);
+
+      
+
+    //   int div = b/a;
+    //   printf()
+   return 0;
+}
